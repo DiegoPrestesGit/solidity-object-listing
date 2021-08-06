5c5fdedfb255873792740232dc482138c6b98430
Diego Prestes <diegoprestes.git@hotmail.com>
2021-08-06 23:33:20 +0000

    feat: remove objects and change it to creditCards

---
+++ contracts/CreditCardList.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+// pragma experimental ABIEncoderV2;
+
+contract CreditCardList {
+  uint public creditCardCount = 0;
+
+  struct CreditCard {
+    uint id;
+    string ownerName;
+    CreaditCardData cardData;
+  }
+
+  struct CreaditCardData {
+    string number;
+    string ownerName;
+    uint validationCode;
+    int creditAvailable;
+    string validationDate;
+    bool blocked;
+  }
+
+  mapping(uint => CreditCard) creditCards;
+
+  function createCreditCard() public {
+    creditCardCount++;
+  }
+}
