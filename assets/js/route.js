dc7cf1bda2d14540e41d3dc4cc098c83e309506b
Diego Prestes <diegoprestes.git@hotmail.com>
2022-04-16 02:16:16 -0300

    manually handling routing

--- assets/js/route.js
+++ assets/js/route.js
@@ -2,18 +2,22 @@ const route = (event) => {
   event = event || window.event;
   event.preventDefault();
   window.history.pushState({}, "", event.target.href);
+  handleLocation();
 };
 
 const routes = {
-  404: "/404.html",
-  "/": "/index.html",
+  404: "/pages/404.html",
+  "/": "/",
+  "/cv": "/pages/cv.html",
+  "/lorem": "/pages/lorem.html",
 };
 
 const handleLocation = async () => {
   const path = window.location.pathname;
   const route = routes[path] || routes[404];
   const html = await fetch(route).then((data) => data.text());
-  document.getElementById("main-page").innerHTML = html;
+  console.log("my html", html);
+  document.getElementById("main-page").innerHTML = "<div></div>";
 };
 
 window.onpopstate = handleLocation;
