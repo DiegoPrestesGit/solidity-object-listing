6e897f6cacf2a0c1a95a18d5ec2174f532466cb2
Diego Prestes <diegoprestes.git@hotmail.com>
2022-04-16 01:29:04 -0300

    finishes off the base

--- assets/js/main.js
+++ assets/js/main.js
@@ -92,18 +92,36 @@ themeButton.addEventListener("click", () => {
 });
 
 /*==================== REDUCE THE SIZE AND PRINT ON AN A4 SHEET ====================*/
+const scaleCv = () => document.body.classList.add("scale-cv");
 
 /*==================== REMOVE THE SIZE WHEN THE CV IS DOWNLOADED ====================*/
+const removeScale = () => document.body.classList.remove("scale-cv");
 
 /*==================== GENERATE PDF ====================*/
 // PDF generated area
+let areaCv = document.getElementById("area-cv");
+let resumeButton = document.getElementById("resume-button");
 
 // Html2pdf options
+const opt = {
+  margin: 0,
+  filename: "diego-prestes-resume.pdf",
+  image: { type: "jpeg", quality: 0.98 },
+  html2canvas: { scale: 4 },
+  jsPDF: { format: "a4", orientation: "portrait" },
+};
 
 // Function to call areaCv and Html2Pdf options
+const generateResume = () => {
+  html2pdf(areaCv, opt);
+};
 
 // When the button is clicked, it executes the three functions
-
+resumeButton.addEventListener("click", () => {
+  scaleCv();
+  generateResume();
+  removeScale();
+});
 // 1. The class .scale-cv is added to the body, where it reduces the size of the elements
 
 // 2. The PDF is generated
