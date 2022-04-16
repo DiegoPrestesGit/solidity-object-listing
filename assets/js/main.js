7acbb2d47cbf4fc4ca999210330465464aa05253
Diego Prestes <diegoprestes.git@hotmail.com>
2022-04-15 23:26:49 -0300

    feat: media queries

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,6 +47,13 @@ const scrollActive = () => {
 window.addEventListener("scroll", scrollActive);
 
 /*==================== SHOW SCROLL TOP ====================*/
+const scrollTop = () => {
+  const scrollTop = document.getElementById("scroll-top");
+  // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if (this.scrollY >= 260) scrollTop.classList.add("show-scroll");
+  else scrollTop.classList.remove("show-scroll");
+};
+window.addEventListener("scroll", scrollTop);
 
 /*==================== DARK LIGHT THEME ====================*/
 
